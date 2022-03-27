cb064f4213e8fc579a58a653dbe314bb8bdf4da4
jd <jdsanchez2011@gmail.com>
2022-03-26 21:08:41 -0600

    configure connection string

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<WebcamDbContext>(
     dbContextOptions => dbContextOptions
-        .UseMySql("Server=localhost;Port=3307;Database=Webcam;Uid=root;Pwd=example;", new MySqlServerVersion(new Version(8, 0, 28)))
+        .UseMySql(builder.Configuration.GetConnectionString("Mysql"), new MySqlServerVersion(new Version(8, 0, 28)))
         // The following three options help with debugging, but should
         // be changed or removed for production.
         .LogTo(Console.WriteLine, LogLevel.Information)
