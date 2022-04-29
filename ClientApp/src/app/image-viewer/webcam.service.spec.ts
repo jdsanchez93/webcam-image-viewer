9ddb949b1adc31e80724c8721f5b6c2b0ce3e10e
jd <jdsanchez2011@gmail.com>
2022-04-29 12:59:31 -0600

    fix webcam service tests

--- ClientApp/src/app/image-viewer/webcam.service.spec.ts
+++ ClientApp/src/app/image-viewer/webcam.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { WebcamService } from './webcam.service';
+import { GarageImage } from './image.models';
+import { HttpClient } from '@angular/common/http';
 
 describe('WebcamService', () => {
   let service: WebcamService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(WebcamService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should patch garage items', () => {
+    const x: Partial<GarageImage> = {
+      numberOfCars: 2
+    }
+    service.patchGarageImage(1, x).subscribe();
+    const req = httpTestingController.expectOne('api/Webcam/1');
+    expect(req.request.method).toEqual('PATCH');
+    expect(req.request.body).toEqual(
+      [{
+        'path': `/numberOfCars`,
+        'op': 'replace',
+        'value': 2
+      }]
+    );
+    req.flush(null);
   });
 });
