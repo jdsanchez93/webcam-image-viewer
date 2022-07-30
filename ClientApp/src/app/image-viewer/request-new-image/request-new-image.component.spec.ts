88d2ea439f3e4caae5c4a5a70510f83ae0e0c77a
jd <jdsanchez2011@gmail.com>
2022-07-30 01:05:00 -0600

    wip for frontend tests

--- ClientApp/src/app/image-viewer/request-new-image/request-new-image.component.spec.ts
+++ ClientApp/src/app/image-viewer/request-new-image/request-new-image.component.spec.ts
@@ -1,20 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { ImageViewerState } from '../state/image-viewer.reducer';
 import { RequestNewImageComponent } from './request-new-image.component';
 
 describe('RequestNewImageComponent', () => {
   let component: RequestNewImageComponent;
   let fixture: ComponentFixture<RequestNewImageComponent>;
+  let store: MockStore;
+  const initialState: ImageViewerState = {
+    loading: false,
+    currentImage: {
+      garageImageId: 0
+    },
+    mostRecentImages: [],
+    webcamSettings: {
+      brightness: 100,
+      contrast: 32
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RequestNewImageComponent ]
+      declarations: [RequestNewImageComponent],
+      providers: [
+        provideMockStore({ initialState }),
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RequestNewImageComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
