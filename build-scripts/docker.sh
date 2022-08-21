ebdaaf9029801cdcb3a63529e028282c39ea985b
jd <jdsanchez2011@gmail.com>
2022-08-21 00:29:35 -0600

    test pushing tag

--- build-scripts/docker.sh
+++ build-scripts/docker.sh
@@ -3,5 +3,7 @@ IMAGE_TAG=$1
 IMAGE_PREFIX="jdeeezy/webcam-image-viewer:"
 IMAGE_NAME="$IMAGE_PREFIX$IMAGE_TAG"
 
-docker build . --file Dockerfile --tag $IMAGE_NAME
-docker push $IMAGE_NAME
+# docker build . --file Dockerfile --tag $IMAGE_NAME
+# docker push $IMAGE_NAME
+
+git push origin $2
