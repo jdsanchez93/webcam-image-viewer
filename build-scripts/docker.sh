2e279d1cb19107505a5535461add2a10940e3356
jd <jdsanchez2011@gmail.com>
2022-08-21 02:18:23 -0600

    conditionally add tag; re-enable docker build

--- build-scripts/docker.sh
+++ build-scripts/docker.sh
@@ -3,11 +3,10 @@ IMAGE_TAG=$1
 IMAGE_PREFIX="jdeeezy/webcam-image-viewer:"
 IMAGE_NAME="$IMAGE_PREFIX$IMAGE_TAG"
 
-# docker build . --file Dockerfile --tag $IMAGE_NAME
-# docker push $IMAGE_NAME
+docker build . --file Dockerfile --tag $IMAGE_NAME
+docker push $IMAGE_NAME
 
-echo $1
-echo $2
-
-# git tag $2
-# git push origin --tags
+if [ ! -z "$2" ]; then
+    git tag $2
+    git push origin --tags
+fi
