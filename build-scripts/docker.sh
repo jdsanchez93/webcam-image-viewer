def89854ae0b6bd6782fd9de02485266b0b9d605
jd <jdsanchez2011@gmail.com>
2022-08-21 23:19:01 -0600

    add comment to force build

--- build-scripts/docker.sh
+++ build-scripts/docker.sh
@@ -6,6 +6,7 @@ IMAGE_NAME="$IMAGE_PREFIX$IMAGE_TAG"
 docker build . --file Dockerfile --tag $IMAGE_NAME
 docker push $IMAGE_NAME
 
+# push tag if second arg is not blank
 if [ ! -z "$2" ]; then
     git tag $2
     git push origin --tags
