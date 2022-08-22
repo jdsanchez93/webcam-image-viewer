5e26d89fe7a6a4342aef6e34473fc414131ce1b0
jd <jdsanchez2011@gmail.com>
2022-08-21 20:52:43 -0600

    temp disable docker build

--- build-scripts/docker.sh
+++ build-scripts/docker.sh
@@ -3,8 +3,8 @@ IMAGE_TAG=$1
 IMAGE_PREFIX="jdeeezy/webcam-image-viewer:"
 IMAGE_NAME="$IMAGE_PREFIX$IMAGE_TAG"
 
-docker build . --file Dockerfile --tag $IMAGE_NAME
-docker push $IMAGE_NAME
+# docker build . --file Dockerfile --tag $IMAGE_NAME
+# docker push $IMAGE_NAME
 
 if [ ! -z "$2" ]; then
     git tag $2
